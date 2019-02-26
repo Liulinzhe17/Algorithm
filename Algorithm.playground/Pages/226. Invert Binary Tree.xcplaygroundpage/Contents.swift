8944d147977abbac798ab192f7d90bb8eaab6215
lzLiu <1015320177@qq.com>
2019-02-26 23:27:51 +0800

    二叉树

---
+++ Algorithm.playground/Pages/226. Invert Binary Tree.xcplaygroundpage/Contents.swift
@@ -0,0 +1,39 @@
+/*
+ Invert a binary tree.
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+
+class Solution {
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        if root == nil { return nil }
+        let resultNode = root
+        
+        let temp = invertTree(resultNode?.left)
+        resultNode?.left = invertTree(resultNode?.right)
+        resultNode?.right = temp
+        
+        return resultNode
+    }
+}
