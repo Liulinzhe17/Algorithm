3e847a5ae2fcabb51e37f863b8a0420b8470b422
lzLiu <1015320177@qq.com>
2019-03-05 22:16:55 +0800

    链表去重

---
+++ Algorithm.playground/Pages/83. Remove Duplicates from Sorted List.xcplaygroundpage/Contents.swift
@@ -0,0 +1,38 @@
+// Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+class Solution {
+    func deleteDuplicates(_ head: ListNode?) -> ListNode? {
+        if head?.next == nil {
+            return head
+        }
+        var node = head
+        while node?.next != nil {
+            if node?.val == node?.next?.val {
+                node?.next = node?.next?.next
+            } else {
+                node = node?.next
+            }
+        }
+        return head
+    }
+}
