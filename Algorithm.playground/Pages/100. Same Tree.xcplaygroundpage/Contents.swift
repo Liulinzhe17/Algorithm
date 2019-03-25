bef8028f5030e7265abbfc43c4d5d87345bd3c55
lzLiu <1015320177@qq.com>
2019-03-25 19:51:13 +0800

    same tree

---
+++ Algorithm.playground/Pages/100. Same Tree.xcplaygroundpage/Contents.swift
@@ -0,0 +1,42 @@
+/*
+ Given two binary trees, write a function to check if they are the same or not.
+ 
+ Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+class Solution {
+    func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        if p == nil && q == nil {
+            return true
+        }
+        
+        if p != nil && q != nil {
+            return (p!.val == q!.val) && isSameTree(p!.left, q!.left) && isSameTree(p!.right, q!.right)
+        }
+        
+        return false
+    }
+}
